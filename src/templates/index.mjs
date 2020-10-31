30b47d0d7fbffc2857798581d2cd59223bbc962e
lowlighter <22963968+lowlighter@users.noreply.github.com>
2020-10-31 17:54:00 +0100

    Sybil template prototype

--- src/templates/index.mjs
+++ src/templates/index.mjs
@@ -2,10 +2,12 @@
 
 //Imports
   import classic from "./classic/template.mjs"
+  import sybil from "./sybil/template.mjs"
   import terminal from "./terminal/template.mjs"
 
 //Exports
   export default {
     classic,
+    sybil,
     terminal,
   }
